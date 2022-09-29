c02db3a2bb6cb6349ca553ca87b2fdf7881abd5a
ccedric <cedric.casareggio@ynov.com>
2022-09-29 12:08:59 +0200

    4

--- test/main.go
+++ test/main.go
@@ -1,11 +1,1 @@
-package main
 
-import (
-	"exam"
-	"fmt"
-)
-
-func main() {
-	fmt.Println(exam.PrintElements([]int{1, 4}, []string{"Choice", "Hello", "Solid", "Curtain", "There", "Forward"}))
-	fmt.Println(exam.PrintElements([]int{2, 0, 7}, []string{"Kenobi", "Unity", "General", "Therapist"}))
-}
