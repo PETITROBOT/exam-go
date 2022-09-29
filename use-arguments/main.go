0a3e17a12557066d60e3c6f38dd479a067ba6798
ccedric <cedric.casareggio@ynov.com>
2022-09-29 12:40:41 +0200

    4

--- use-arguments/main.go
+++ use-arguments/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "regexp"
-    "strconv"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
-    for i, arg := range os.Args[1:] {
-        if !regexp.MustCompile(`/d`).MatchString(arg) {
-            fmt.Println("Argument " + strconv.Itoa(i+1) + " : " + arg)
-        }
-    }
+	for i, arg := range os.Args[1:] {
+		if !regexp.MustCompile(`/d`).MatchString(arg) {
+			fmt.Println("Argument " + strconv.Itoa(i+1) + " : " + arg)
+		}
+	}
 }
